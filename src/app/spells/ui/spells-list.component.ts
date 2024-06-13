a6d7d56723c52e1de83748349f7fc44ae8457ba2
ArdeleanRazvan <ardelean.razvan.c@gmail.com>
2024-06-13 21:13:25 +0300

    Added fonts and started to create the layout for spells

--- src/app/spells/ui/spells-list.component.ts
+++ src/app/spells/ui/spells-list.component.ts
@@ -5,27 +5,38 @@ import { Spell } from '../../../shared/interfaces/spell';
   standalone: true,
   selector: 'dnd-spells-list',
   template: `
-    <div class="join join-vertical m-1 w-full text-white">
-      @for (spell of spells(); track spell.name) {
-        <div
-          class="collapse join-item collapse-arrow border-2 border-brown-500"
-        >
-          <input type="checkbox" name="{{ spell.name }}" />
-          <div class="collapse-title">
-            <div class="flex justify-start">
-              <img
-                class="w-8"
-                src="./assets/spells/Spell_Evocation_EldritchBlast.webp"
-                alt=""
-              />
-              <p class="sm:text:xl text-md font-medium">{{ spell.name }}</p>
+    <div
+      class="join join-vertical m-1 flex w-full bg-gradient-to-r from-amber-800 via-amber-400 to-amber-800 text-white"
+    >
+      <div class="mx-0.5 rounded-lg">
+        @for (spell of spells(); track spell.name) {
+          <div class="collapse join-item collapse-arrow my-0.5 bg-brown-900">
+            <input type="checkbox" name="{{ spell.name }}" />
+            <div class="collapse-title">
+              <div class="flex flex-col">
+                <div class="flex">
+                  <img
+                    class="w-8"
+                    src="./assets/spells/Spell_Evocation_EldritchBlast.webp"
+                    alt=""
+                  />
+                  <p class="sm:text:xl font-alegreya text-lg font-extrabold">
+                    {{ spell.name }}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <p class="text-md font-alegreya font-extrabold text-brown-200">
+                  Level 1 Evocation Spell
+                </p>
+              </div>
+            </div>
+            <div class="collapse-content">
+              <p>Collapse Content</p>
             </div>
           </div>
-          <div class="collapse-content">
-            <p>Collapse Content</p>
-          </div>
-        </div>
-      }
+        }
+      </div>
     </div>
   `,
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         "brown-500": "hsl(15,60%,60%)",
         "brown-900": "#1c1817",
       },
+      fontFamily: {
+        alegreya: ["Alegreya", "sans-serif"],
+      },
     },
   },
   plugins: [require("daisyui")],
